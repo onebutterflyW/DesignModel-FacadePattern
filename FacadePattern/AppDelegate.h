42065815ecc3e1e2e9377912e2c6d5a5ffbdffd2
415 <a415@415demacbook-pro.local>
2017-01-20 11:07:41 +0800

    Initial Commit

---
+++ FacadePattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FacadePattern
+//
+//  Created by 415 on 17/1/20.
+//  Copyright © 2017年 415. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FacadePattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FacadePattern
+//
+//  Created by 415 on 17/1/20.
+//  Copyright © 2017年 415. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
